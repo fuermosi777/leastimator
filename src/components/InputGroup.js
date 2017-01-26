3b4bacd2eedee4b3d0c50b921435afa95585505c
fuermosi777 <liuhao1990@gmail.com>
2017-01-25 22:13:24 -0800

    optimize add car

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -4,17 +4,14 @@ import {
   TextInput,
   StyleSheet,
   Text,
-  TouchableHighlight,
+  Alert,
 } from 'react-native';
 import {COLOR, INPUT_GROUP_TYPE} from '../constants';
+import moment from 'moment';
 
 export default class T extends Component {
   constructor(props) {
     super(props);
-  }
-
-  state = {
-    value: '',
   }
 
   render() {
@@ -23,7 +20,7 @@ export default class T extends Component {
         <Text style={styles.label}>{this.props.label}</Text>
         <TextInput
           style={styles.input}
-          value={this.state.value}
+          value={this.props.value}
           placeholder={this.props.placeholder}
           placeholderTextColor={COLOR.SECONDARY}
           keyboardType={this.props.type === INPUT_GROUP_TYPE.INTEGER ? 'numeric' : 'default'}
@@ -33,28 +30,62 @@ export default class T extends Component {
           dataDetectorTypes="none"
           spellCheck={false}
           autoComplete={false}
-          onKeyPress={this.handleKeyPress}
         />
       </View>
     );
   }
 
   handleChangeText = (text) => {
-    if (!this.isValidate(text)) {
+    if (!this.isAllowed(text)) {
       text = text.slice(0, -1);
     }
+    this.props.onChangeText(text);
     this.setState({value: text});
   }
 
-  handleKeyPress = (e) => {
-    // var key = e.nativeEvent.key;
-  }
-
-  isValidate(value) {
+  isAllowed(value) {
     if (this.props.type === INPUT_GROUP_TYPE.INTEGER) {
       return /^\d+$/.test(value);
     }
+    if (this.props.type === INPUT_GROUP_TYPE.TEXT) {
+      return true;
+    }
+    if (this.props.type === INPUT_GROUP_TYPE.DATE) {
+      return /^[0-9\/]+$/.test(value);
+    }
   }
+
+  // checkValidate(value) {
+  //   if (this.props.type === INPUT_GROUP_TYPE.INTEGER) {
+  //     if (!/^\d+$/.test(value)) {
+  //       throw new Error('Only integer is allowed');
+  //     }
+  //   }
+  //   if (this.props.type === INPUT_GROUP_TYPE.DATE) {
+  //     let formatOK = /^((0?[13578]|10|12)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[01]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1}))|(0?[2469]|11)(-|\/)(([1-9])|(0[1-9])|([12])([0-9]?)|(3[0]?))(-|\/)((19)([2-9])(\d{1})|(20)([01])(\d{1})|([8901])(\d{1})))$/.test(value);
+  //     if (!formatOK) {
+  //       throw new Error('The date format should be MM/DD/YYYY');
+  //     }
+  //     var date = moment(value, 'MM/DD/YYYY');
+  //     var today = moment();
+  //     if (today < date) {
+  //       throw new Error('Only past date is allowed');
+  //     }
+  //   }
+  // }
+
+  // handleSubmit = () => {
+  //   let value = this.state.value;
+  //   if (!value) {
+  //     Alert.alert('Error', 'Please enter information', [{text: 'OK'}]);
+  //   }
+  //   try {
+  //     this.checkValidate(value);
+  //   } catch (err) {
+  //     Alert.alert('Error', err.message, [{text: 'OK'}]);
+  //     return;
+  //   }
+  // }
 
 }
 
--- src/scenes/AddCarScene.js
+++ src/scenes/AddCarScene.js
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import {
   StyleSheet,
   KeyboardAvoidingView,
+  Text,
+  View,
+  TouchableHighlight,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {COLOR, INPUT_GROUP_TYPE} from '../constants';
@@ -18,6 +21,37 @@ export default class AddCarScene extends Component {
     this.state = {
       selectedCarIconName: DEFAULT_CAR_ICON_NAME,
       showDatePicker: false,
+      inputs: [{
+        name: 'nickname',
+        value: '',
+        label: 'Nickname',
+        placeholder: 'My car',
+        type: INPUT_GROUP_TYPE.TEXT,
+      }, {
+        name: 'startingMiles',
+        value: '',
+        label: 'Starting Miles',
+        placeholder: '20',
+        type: INPUT_GROUP_TYPE.INTEGER,
+      }, {
+        name: 'milesAllowed',
+        value: '',
+        label: 'Miles Allowed',
+        placeholder: '30000',
+        type: INPUT_GROUP_TYPE.INTEGER,
+      }, {
+        name: 'lengthOfLease',
+        value: '',
+        label: 'Length of Lease',
+        placeholder: '36',
+        type: INPUT_GROUP_TYPE.INTEGER,
+      }, {
+        name: 'leaseStartDate',
+        value: '',
+        label: 'Lease Start Date',
+        placeholder: todayDate(),
+        type: INPUT_GROUP_TYPE.DATE,
+      }]
     };
   }
 
@@ -36,16 +70,26 @@ export default class AddCarScene extends Component {
               }
             }));}}
           />
-          <InputGroup label="Nickname" placeholder="My car" type={INPUT_GROUP_TYPE.TEXT}/>
-          <InputGroup label="Starting Miles" placeholder="20" type={INPUT_GROUP_TYPE.INTEGER}/>
-          <InputGroup label="Miles Allowed" placeholder="30000" type={INPUT_GROUP_TYPE.INTEGER}/>
-          <InputGroup label="Length of Lease" placeholder="36" type={INPUT_GROUP_TYPE.INTEGER}/>
-          <InputGroup 
-            label="Lease Start Date" 
-            type={INPUT_GROUP_TYPE.DATE}
-            placeholder={todayDate()}
-          />
+          {this.state.inputs.map((item, key) => {
+            return (
+              <InputGroup 
+                key={key}
+                value={item.value} 
+                label={item.label}
+                placeholder={item.placeholder} 
+                type={item.type}
+                onChangeText={this.handleInputTextChange.bind(this, key)}
+              />
+            );
+          })}
         </KeyboardAvoidingView>
+        <View style={styles.save}>
+          <TouchableHighlight
+            onPress={this.handleSavePress}
+          >
+            <Text style={styles.text}>SAVE</Text>
+          </TouchableHighlight>
+        </View>
       </LinearGradient>
     );
   }
@@ -54,12 +98,33 @@ export default class AddCarScene extends Component {
     this.setState({selectedCarIconName});
   }
 
+  handleInputTextChange = (key, text) => {
+    let state = this.state;
+    state.inputs[key].value = text;
+  }
+
+  handleSavePress = () => {
+    // TODO
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  save: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 60,
+  },
+  text: {
+    textAlign: 'center',
+    color: COLOR.WHITE,
+    fontSize: 16,
+    fontWeight: '300',
+  }
 });
 
 AddCarScene.propTypes = {
